3b788c1fa0fcb8aa2d8bfb7bc162d56cc9b353c3
Flash007-xd <163484366+Flash007-xd@users.noreply.github.com>
2024-09-06 17:29:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+// // alert ("Hello World")
+
+// var firstname = "Shahmeer";
+
+// var lastname = "Muhammad";
+
+// var city = "Karachi";
+
+// var education = "bachelors"
+
+
+// var num1 = 10;
+// var num2 = 50;
+
+// console.log(num1 + num2) ;
+
+
+// document.write("Hello my name is "+ firstname+" " +lastname +". I live in " + city +" and my education is " + education +".");
+
+
+// // console.log()
+ 
+
+// // --------------------------------------------------------------------------------
+
+
+// //variables
+// Number 
+// String
+
+
+// console.log("Hello my name is "+ firstname+" " +lastname +". I live in " + city +" and my education is " + education + ".") ;
+
+// _________________________________________________________Chapter 1 (ALERTS)_______________________________________________
+
+// TASK 1
+
+alert("Error! Please enter a valid password.")
+
+// TASK 2
+alert("Welcome to JS Land..,\nHappy Coding!")
+// TASK 3
+alert("Welcome to JS Land...")
+
+// TASK 4
+alert("Happy Coding!")
+
+// TASK 5
+console.log("Hello... I can run JS through my web browser's console")
+
+// TASK 6
+
+// All done
+
+// _____________________________________________Chapter 2 Variable for Strings____________________________________
+// TASK 1
+var userName;
+// TASK 2
+var myName = "Shahmeer Muhammad"
+// TASK 3 
+var message = "Hello World"
+alert(message)
+// TASK 4 
+// STUDENT BIO DATA 
+var studentName = "Jhone Doe"
+var age = "15 years old"
+var occupation = "Certified Mobile Application Development"
+
+alert(studentName)
+alert(age)
+alert(occupation)
+
+// TASK 5
+
+var something = "PIZZA \nPIZZ \nPIZ\nPI\nP"
+alert(something)
+
+// TASK 6
+var email = "shahmeermuhd23@gmail.com"
+alert("My email address is "+ email)
+
+// TASK 7
+var book = "A smarter way to learn JavaScript."
+alert("I am trying to learn from the Book "+book)
+
+// TASK 8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// TASK 9
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
+
+
+// _______________________________________________Chapter 3(Variable for Numbers)_____________________________________________
+// TASK 1
+
+var age1 = "20"
+alert("I am "+age1+" years old")
+
+// TASK 2 
+// declration
+var visits
+// Initialization
+var visits = "14"
+
+alert("You have visited this site "+visits+" times")
+
+// TASK 3
+var birthYear = 2003
+document.write("My Birth Year is "+birthYear + "<br> Data type of my declared variable is "+typeof birthYear +"<br>")
+
+// TASK 4
+var storeWebsite = "www.xyzClothing.com"
+var Visitor_Name = "<b>Jhone Doe" 
+var productTitle = "T-Shirt"
+var Quantity = 5
+
+document.write(Visitor_Name
+    + " <b>ordered "
+    + Quantity
+    + " "
+    + productTitle
+    + "(s) on "
+    + storeWebsite
+)
+
+// ________________________________________________Chapter4(Variable Names: Legal & Illegal)________________________________________
+
+// TASK 1
+var age , education , userName;
+
+// // TASK 2
+// Legal________
+var userName
+var user_Name
+var totalAmount
+var isValid
+var filePath
+
+// // Illegal_______
+// var 2ndPlace
+// var user name
+// var if
+// var total-amount
+// var int
+
+// TASK 3 (a)
+document.write("<h1>Rules for Naming JS Variables</h1>");
+
+// (b)
+document.write("Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable<br>");
+
+// (c)
+document.write("Variable names must begin with a letter, $, or _. For example: $name, _name, or name<br>");
+
+// (d)
+document.write("Variable names are case sensitive.<br>");
+
+// (e)
+document.write("Variable names should not be JavaScript keywords.<br>");
+
+
